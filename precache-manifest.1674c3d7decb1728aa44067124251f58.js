1ae80c079b91e7acf33ba8238b4751375463e856
Lukas Unzeitig <lukas.unzeitig@gmail.com>
2021-05-31 13:33:49 +0200

    Updates

---
+++ precache-manifest.1674c3d7decb1728aa44067124251f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d100319789932bb458ef02fb7a25612e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ca1f55eafeb180b3f0d",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "c281d58e407a5a51765a",
+    "url": "./static/js/2.b5c15224.chunk.js"
+  },
+  {
+    "revision": "7d88dc74bf2fb2b185f8e0016dbdaf20",
+    "url": "./static/js/2.b5c15224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca1f55eafeb180b3f0d",
+    "url": "./static/js/main.ce785a7f.chunk.js"
+  },
+  {
+    "revision": "f1d47144f2a689d15823",
+    "url": "./static/js/runtime-main.4831a401.js"
+  }
+]);
